1249f0e64fe2a0a9c1faeb997f444652cd04f3b8
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-14 23:08:17 -0200

    revisão inicial do tema Pensando

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="application-name" content="">
+    <meta name="msapplication-TileColor" content="">
+    <link rel="shortcut icon" href="images/favicon-pd.jpg">
+    <title>Portal do Projeto Pensando o Direito - Marco civil da intenet</title>
+
+    <!-- styles gerais do portal -->
+    <link href="styles.css" rel="stylesheet">
+
+    <!-- HTML5Shim e Respond.js oferecem suporte HTML5 e media queries para o IE8 -->
+    <!-- AVISO: Respond.js não funciona localmente ("//file:...") -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- <div id="barra-brasil" style="background:#7F7F7F; height: 20px; padding:0 0 0 10px; display:block;" class="clearfix">
+        <ul id="menu-barra-temp" style="list-style:none;">
+            <li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED"><a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>
+            </li>
+            <li><a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a>
+            </li>
+        </ul>
+    </div> -->
+    <div class="container">
+        <div class="header">
+            <div class="col-xs-3">
+                <div id="logo-principal">
+                    <a href="#">
+                        <h1>Portal do Projeto Pensando o Direito</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-9 pull-right" id="navegacao-top">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-top">
+                                <i class="fa fa-caret-square-o-down"></i> Menu
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="menu-top">
+
+                            <form class="navbar-form navbar-right" role="search" id="busca">
+                                <div class="input-group busca-pd">
+                                    <input type="text" class="form-control" placeholder="Buscar no site" name="srch-term" id="srch-term">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit"><i class="fa fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Notícias</a> </li>
+                                <li><a href="#">Editais</a> </li>
+                                <li><a href="#">Parceiros</a> </li>
+                                <li><a href="#">Fale conosco</a> </li>
+                            </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div id="navegacao-destaque">
+        <div class="container">
+            <ul class="navegacao-destaque-list">
+                <li class="navegacao-destaque-item">
+                    <div class="navegacao-destaque-content">
+                        <h5><a href="#">Conheça o projeto</a></h5>
+                        <p><a href="#">Conheça também a Secretaria de Assuntos Legislativos (SAL)</a>
+                        </p>
+                    </div>
+                </li>
+                <li class="navegacao-destaque-item">
+                    <div class="navegacao-destaque-content">
+                        <h5><a href="#">Publicações</a></h5>
+                        <p><a href="#">Conheça também a Secretaria de Assuntos Legislativos (SAL)</a>
+                        </p>
+                    </div>
+                </li>
+                <li class="navegacao-destaque-item">
+                    <div class="navegacao-destaque-content">
+                        <h5><a href="#">Debates</a></h5>
+                        <p><a href="#">Sua participação é muito importante</a>
+                        </p>
+                    </div>
+                </li>
+                <li class="navegacao-destaque-item participe">
+                    <div class="arrow-right"></div>
+                    <div class="navegacao-destaque-content">
+                        <h5><a href="#">Participe!</a></h5>
+                        <p><a href="#">Cadastre-se</a>
+                            <br /><a href="#">Já é cadastrado?</a>
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="conteudo">
+        <div class="container mt-sm">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="font-roboto red">Debates Públicos</h2>
+                </div>
+                <div class="col-md-6 text-right">
+                    <p>
+                        <button type="button" class="btn btn-danger">Participe!</button>
+                        <strong class="mt-xs ml-md"><a href="#">Cadastre-se</a> | <a href="#">Já é cadastrado?</a></strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. </p>
+            <div class="row mt-md" id="debates">
+                <div class="col-md-6">
+                    <div class="debate-item protecao">
+                        <div class="text-center">
+                            <img src="images/logos/protecao-w.png" class="img-responsive" alt="Proteção de Dados Pessoais">
+                        </div>
+                        <div class="description">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. </p>
+                        </div>
+                    </div>
+                    <div class="mt-sm text-center">
+                        <button type="button" class="btn btn-danger font-roboto">Participe deste debate!</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="debate-item marco-civil">
+                        <div class="text-center">
+                            <img src="images/logos/marcocivil-b.png" class="img-responsive" alt="Proteção de Dados Pessoais">
+                        </div>
+                        <div class="description">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. </p>
+                        </div>
+                    </div>
+                    <div class="mt-sm text-center">
+                        <button type="button" class="btn btn-danger font-roboto">Participe deste debate!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-md">
+            <div id="mini-tuto">
+                <h4 class="font-roboto red">Como participar?</h4>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="tuto-pass">
+                            <div class="text-center">
+                                <img src="images/tuto/tuto-01.png" class="img-responsive" alt="Cadastre-se no site">
+                                <p class="font-amatic h5">Cadastre-se no site</p>
+                            </div>
+                            <div class="tuto-text">
+                                <p><a href="#">Cadastre-se</a> | <a href="#">Já é cadastrado?</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="tuto-pass">
+                            <div class="text-center">
+                                <img src="images/tuto/tuto-02.png" class="img-responsive" alt="Confirme o cadastro">
+                                <p class="font-amatic h5">Confirme o cadastro</p>
+                            </div>
+                            <div class="tuto-text">
+                                <p>Vai chegar no seu email, é só clicar no link</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="tuto-pass">
+                            <div class="text-center">
+                                <img src="images/tuto/tuto-03.png" class="img-responsive" alt="Escolha">
+                                <p class="font-amatic h5">Escolha </p>
+                            </div>
+                            <div class="tuto-text">
+                                <p>O debate que você quer participar</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="tuto-pass">
+                            <div class="text-center">
+                                <img src="images/tuto/tuto-04.png" class="img-responsive" alt="Cadastre-se no site">
+                                <p class="font-amatic h5">Pronto! </p>
+                            </div>
+                            <div class="tuto-text">
+                                <p>Deixe seu comentário, crítica etc</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <img src="images/structure/logo-pd-gray.png" class="img-responsive" alt="Logo: Projeto Pensando o Direito">
+                </div>
+                <div class="col-sm-5 footer-menu">
+                    <ul class="list-inline mt-sm">
+                        <li><a href="">CONHEÇA O PROJETO</a>
+                        </li>
+                        <li><a href="">DEBATES</a>
+                        </li>
+                        <li><a href="">PUBLICAÇÕES</a>
+                        </li>
+                    </ul>
+                    <ul class="list-inline footer-menu">
+                        <li><a href="">Notícias</a>
+                        </li>
+                        <li><a href="">Editais</a>
+                        </li>
+                        <li><a href="">Parceiros</a>
+                        </li>
+                        <li><a href="">Fale conosco</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-5">
+                    <ul class="list-inline mt-sm">
+                        <li>
+                            <a href=""><img src="images/logos/ipea.png" class="img-responsive" alt="Ipea">
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="images/logos/pnud.png" class="img-responsive" alt="PNUD">
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="images/logos/sal.png" class="img-responsive" alt="Secretaria de assuntos legislativos">
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="images/logos/mj.png" class="img-responsive" alt="Ministério da Justiça">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row text-center mt-md fontsize-xs">
+                <p>Todo o conteúdo do Projeto Pensando o Direito está licenciado sob a CC-by-sa-2.5, exceto quando especificado em contrário e nos conteúdos replicados de outras fontes.</p>
+            </div>
+        </div>
+    </div>
+    <!-- Js da Barra Brasil -->
+    <!--<script defer="defer" async="async" src="js/barra.js" type="text/javascript"></script>  -->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+
+    <!-- Todos os js's do Bootstrap, compilados -->
+    <script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
+
+    <!-- Core da pagina -->
+    <script type="text/javascript" src="js/core-mci.js"></script>
+
+
+</body>
+
+</html>
