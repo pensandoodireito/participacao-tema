1c8c0c6a81854f9789fb44e60142f904ce004565
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-19 22:55:25 -0200

    Alteracoes html

--- mini-tutorial.php
+++ mini-tutorial.php
@@ -1,51 +1,51 @@
 <div class="container mt-md">
-<div id="mini-tuto">
-    <h4 class="font-roboto red">Como participar?</h4>
-    <div class="row">
-        <div class="col-sm-3">
-            <div class="tuto-pass">
-                <div class="text-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-01.png" class="img-responsive" alt="Cadastre-se no site">
-                    <p class="font-amatic h5">Cadastre-se no site</p>
+    <div id="mini-tuto">
+        <h4 class="font-roboto red">Como participar?</h4>
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="tuto-pass">
+                    <div class="text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-01.png" class="img-adptive" alt="Cadastre-se no site">
+                        <p class="font-amatic h5">Cadastre-se no site</p>
+                    </div>
+                    <div class="tuto-text">
+                        <p><a href="#">Cadastre-se</a> | <a href="#">Já é cadastrado?</a></p>
+                    </div>
                 </div>
-                <div class="tuto-text">
-                    <p><a href="#">Cadastre-se</a> | <a href="#">Já é cadastrado?</a></p>
+            </div>
+            <div class="col-sm-3">
+                <div class="tuto-pass">
+                    <div class="text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-02.png" class="img-adptive" alt="Confirme o cadastro">
+                        <p class="font-amatic h5">Confirme o cadastro</p>
+                    </div>
+                    <div class="tuto-text">
+                        <p>Vai chegar no seu email, é só clicar no link</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="tuto-pass">
+                    <div class="text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-03.png" class="img-adptive" alt="Escolha">
+                        <p class="font-amatic h5">Escolha </p>
+                    </div>
+                    <div class="tuto-text">
+                        <p>O debate que você quer participar</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="tuto-pass">
+                    <div class="text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-04.png" class="img-adptive" alt="Cadastre-se no site">
+                        <p class="font-amatic h5">Pronto! </p>
+                    </div>
+                    <div class="tuto-text">
+                        <p>Deixe seu comentário, crítica etc</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-3">
-            <div class="tuto-pass">
-                <div class="text-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-02.png" class="img-responsive" alt="Confirme o cadastro">
-                    <p class="font-amatic h5">Confirme o cadastro</p>
-                </div>
-                <div class="tuto-text">
-                    <p>Vai chegar no seu email, é só clicar no link</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="tuto-pass">
-                <div class="text-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-03.png" class="img-responsive" alt="Escolha">
-                    <p class="font-amatic h5">Escolha </p>
-                </div>
-                <div class="tuto-text">
-                    <p>O debate que você quer participar</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="tuto-pass">
-                <div class="text-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-04.png" class="img-responsive" alt="Cadastre-se no site">
-                    <p class="font-amatic h5">Pronto! </p>
-                </div>
-                <div class="tuto-text">
-                    <p>Deixe seu comentário, crítica etc</p>
-                </div>
-            </div>
-        </div>
-    </div>
     </div>
 </div>
