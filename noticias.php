99cf3b8ee1400d39fabc22eb4e711e02652aa6c0
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-20 11:10:13 -0200

    Inclusão do código para a área de notícias; Exclusão de elementos da antiga interface;

--- noticias.php
+++ noticias.php
@@ -1,15 +1,125 @@
-<div class="container mt-lg">
-	<div class="row">
-		<div class="col-xs-12">
-			<h4 class="font-roboto red"><strong>Notícias - Proteção de Dados</strong></h4>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6">
-			<div class="row">
-				
+<div class="noticias">
+	<div class="container mt-lg">
+		<div class="row">
+			<div class="col-xs-12">
+				<h4 class="font-roboto red"><strong>Notícias - Proteção de Dados</strong></h4>
 			</div>
 		</div>
-		<div class="col-md-6"></div>
+		<div class="row">
+			<div class="col-sm-6 col-xs-12">
+				<div class="noticia-destaque">
+					<div class="imagem-destaque">
+						<img class="img-adptive" src="<?php echo get_template_directory_uri(); ?>/images/home/temp/img-marcacao.jpg">
+					</div>
+					<div class="texto-destaque">
+						<p class="h5 red">
+						<strong>
+						Equipe da pesquisa faz sua primeira reunião
+						</strong>
+						</p>
+						<p>Neste mês, ocorreu a primeira reunião entre a equipe do Projeto Pensando o Direito e os pesquisadores selecionados no edital  “Diagnóstico dos serviços prisionais no Brasil” </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				<div class="tabs-social">
+					<ul id="tabs" class="nav tabs-pd" data-tabs="tabs">
+						<li class="active">
+							<a href="#Twitter" data-toggle="tab" class="font-roboto">Twitter</a>
+							<div class="tab-arrow"></div>
+						</li>
+						<li>
+							<a href="#Facebook" data-toggle="tab" class="font-roboto">Facebook</a>
+							<div class="tab-arrow"></div>
+						</li>
+					</ul>
+					<div id="" class="tab-content">
+						<div class="tab-pane active" id="Twitter">
+							<a class="twitter-timeline" height="350" href="https://twitter.com/projetopd" data-widget-id="527542191929827328">Tweets do Projeto pensando o direito - @projetopd</a>
+							<script>
+							! function(d, s, id) {
+							var js, fjs = d.getElementsByTagName(s)[0],
+							p = /^http:/.test(d.location) ? 'http' : 'https';
+							if (!d.getElementById(id)) {
+							js = d.createElement(s);
+							js.id = id;
+							js.src = p + "://platform.twitter.com/widgets.js";
+							fjs.parentNode.insertBefore(js, fjs);
+							}
+							}(document, "script", "twitter-wjs");
+							</script>
+						</div>
+						<div class="tab-pane" id="Facebook">
+							<p>facebook app</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-6 col-xs-12">
+				<div class="row  mt-md">
+					<div class="col-xs-5">
+						<img class="img-adptive" src="<?php echo get_template_directory_uri(); ?>/images/home/temp/img-marcacao.jpg">
+					</div>
+					<div class="col-xs-7 pl-0">
+						<p class="h6 red mt-0">
+						<strong>
+						<a href="#">Seminário Internacional revelam o uso do celular e aplicativos no reconhecimento de suspeitos no Brasil</a>
+						</strong>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				<div class="row mt-md">
+					<div class="col-xs-5">
+						<img class="img-adptive" src="<?php echo get_template_directory_uri(); ?>/images/home/temp/img-marcacao.jpg">
+					</div>
+					<div class="col-xs-7 pl-0">
+						<p class="h6 red mt-0">
+						<strong>
+						<a href="#">Seminário Internacional revelam o uso do celular e aplicativos no reconhecimento de suspeitos no Brasil</a>
+						</strong>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-6 col-xs-12">
+				<div class="row mt-md">
+					<div class="col-xs-5">
+						<img class="img-adptive" src="<?php echo get_template_directory_uri(); ?>/images/home/temp/img-marcacao.jpg">
+					</div>
+					<div class="col-xs-7 pl-0">
+						<p class="h6 red mt-0">
+						<strong>
+						<a href="#">Seminário Internacional revelam o uso do celular e aplicativos no reconhecimento de suspeitos no Brasil</a>
+						</strong>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				<div class="row mt-md">
+					<div class="col-xs-5">
+						<img class="img-adptive" src="<?php echo get_template_directory_uri(); ?>/images/home/temp/img-marcacao.jpg">
+					</div>
+					<div class="col-xs-7 pl-0">
+						<p class="h6 red mt-0">
+						<strong>
+						<a href="#">Seminário Internacional revelam o uso do celular e aplicativos no reconhecimento de suspeitos no Brasil</a>
+						</strong>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mt-md">
+			<p>
+			<a href="#" class="blue"><strong>Todas as notícias</strong></a>
+			</p>
+		</div>
 	</div>
 </div>
