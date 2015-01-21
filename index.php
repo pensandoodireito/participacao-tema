fec2bd0efbf58fc9b9e731f9547add077151c993
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-21 17:39:52 -0200

    modificação da estrutura do tema para forçar o front-page

--- index.php
+++ index.php
@@ -1,49 +1,64 @@
-<?php get_header(); ?>
-        <div class="container">
-            <div class="row mt-md" id="debates">
-                <div class="col-md-6">
-                    <div class="debate-item protecao">
-                        <div class="text-center">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/protecao-w.png" class="img-adptive" alt="Proteção de Dados Pessoais">
-                </div>
-                <div class="description">
-                        <?php
-                            $page_dadospessoais = get_page_by_title('Proteção de Dados Pessoais');
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
 
-                            if ($page_dadospessoais != null) {
-                                echo $page_dadospessoais->post_content;
-                            }
-                        ?>
-                </div>
-            </div>
-            <div class="mt-sm text-center">
-                <a href="<?php echo site_url("/dadospessoais"); ?>" class="btn btn-danger font-roboto">Participe deste debate!</a>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="debate-item marco-civil">
-                <div class="text-center">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/marcocivil-b.png" class="img-adptive" alt="Proteção de Dados Pessoais">
-                </div>
-                <div class="description">
-                        <?php
-                        $page_marcocivil = get_page_by_title('Marco Civil da Internet');
+get_header(); ?>
 
-                        if ($page_marcocivil != null) {
-                            echo $page_marcocivil->post_content;
-                        }
-                        ?>
-                </div>
-            </div>
-            <div class="mt-sm text-center">
-                <a href="<?php echo site_url("/marcocivil"); ?>" class="btn btn-danger font-roboto">Participe deste debate!</a>
-            </div>
-        </div>
-    </div>
-</div>
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-<?php get_template_part('mini-tutorial'); ?>
+		<?php if ( have_posts() ) : ?>
 
-<?php get_template_part('noticias'); ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
 
 <?php get_footer(); ?>
