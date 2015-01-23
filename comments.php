a2b39c21fa10d0510bfa9508912fdd140b9dbdba
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-22 22:44:13 -0200

    correção no template de comentário

--- comments.php
+++ comments.php
@@ -9,12 +9,10 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'twentyfifteen' ),
+				printf( _nx( 'Um comentário para &ldquo;%2$s&rdquo;', '%1$s comentários em &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title' ),
 					number_format_i18n( get_comments_number() ), get_the_title() );
 			?>
 		</h2>
-
-		<?php twentyfifteen_comment_nav(); ?>
 
 		<ol class="comment-list">
 			<?php
@@ -26,15 +24,13 @@ if ( post_password_required() ) {
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php twentyfifteen_comment_nav(); ?>
-
 	<?php endif; // have_comments() ?>
 
 	<?php
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfifteen' ); ?></p>
+		<p class="no-comments"><?php _e( 'Comments are closed.' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
