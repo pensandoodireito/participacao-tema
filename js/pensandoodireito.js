de12e64a037f8f22620519092040e1de503908f8
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-25 19:27:45 -0200

    ajustada variaveis para escapar do conflito de jquery

--- js/pensandoodireito.js
+++ js/pensandoodireito.js
@@ -12,19 +12,48 @@
  });
  });*/
 
-function sticky_relocate($) {
-    var window_top = $(window).scrollTop();
-    if ($('#fixa-menu-eixo').length > 0) {
-        var div_top = $('#fixa-menu-eixo').offset().top;
+function sticky_relocate() {
+    var window_top = jQuery(window).scrollTop();
+    if (jQuery('#fixa-menu-eixo').length > 0) {
+        var div_top = jQuery('#fixa-menu-eixo').offset().top;
         if (window_top > div_top) {
-            $('#menueixos-sticker').addClass('menueixo-stick');
+            jQuery('#menueixos-sticker').addClass('menueixo-stick');
         } else {
-            $('#menueixos-sticker').removeClass('menueixo-stick');
+            jQuery('#menueixos-sticker').removeClass('menueixo-stick');
         }
     }
 }
 
+function back_to_top($) {
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, duration);
+        return false;
+    })
+}
+
 jQuery(function ($) {
     $(window).scroll(sticky_relocate);
-    sticky_relocate($);
+    sticky_relocate();
+
+    // botão que fecha o menu
+    $('.fecha-menu').click(function() {
+        $('.indice .menu').slideToggle('slow');
+        $(".fecha-menu .fa").toggleClass('fa-angle-up fa-angle-down');
+    });
+
+    back_to_top($);
+
 });
