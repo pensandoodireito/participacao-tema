823e6b567ed85c0fd1fe02a2963d828caa890ae5
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-02-03 10:53:36 -0200

    adicionado os elementos para inclusão da barra Brasil no debate

--- footer.php
+++ footer.php
@@ -58,6 +58,8 @@
 
 <?php wp_footer(); ?>
 
+<script defer="defer" async="async" src="//barra.brasil.gov.br/barra.js" type="text/javascript"></script>
+
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
