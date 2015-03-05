2430a9648fb5a07f09f25dfdc58661daae825ada
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-05 13:03:43 -0300

    criação dos tempaltes de publicacao

---
+++ content-publicacao.php
@@ -0,0 +1,8 @@
+<div class="container content-noticia">
+	<div class="row">
+    <h4 class="red"><strong><?php the_title(); ?></strong></h4>
+    <?php the_content(); ?>
+    </div>
+    <?php get_sidebar(); ?>
+    <h1>EU SOU UM TEMPLATE DE PUBLICAÇÃO!!</h1>
+</div>
