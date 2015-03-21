ab1c037de9cbfdc4bc9e3948b434e9fa83c29e60
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-21 16:02:07 -0300

    modificação do breadcrum para mostrar o nome quando for a página do archive do post type

--- functions.php
+++ functions.php
@@ -396,7 +396,7 @@ function wp_custom_breadcrumbs() {
 
         } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
             $post_type = get_post_type_object(get_post_type());
-            echo $before . $post_type->labels->singular_name . $after;
+            echo $before . $post_type->labels->name . $after;
 
         } elseif ( is_attachment() ) {
             $parent = get_post($post->post_parent);
