43ebff0af68005295a7cdbd7ceb51d7cddc5da46
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-26 19:14:57 -0300

    ajusts com base nas sugestões do Thiago via slack

--- content-single.php
+++ content-single.php
@@ -1,6 +1,6 @@
 
 	<div class="row">
-		<div class="col-xs-8 text-left">
+		<div class="col-md-8 text-left">
 		    <h4 class="red"><strong><?php the_title(); ?></strong></h4>
 		    <p><mark><?php the_date('d \d\e F \d\e Y'); ?></mark></p>
 		    <?php the_content(); ?>
