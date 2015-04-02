7f8649b2a0d59b34cb069daa2e1d7fe5db2be192
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-04-02 16:40:32 -0300

    Removendo menu-horizontal que não será mais utilizado

--- page.php
+++ page.php
@@ -13,11 +13,6 @@
 
 get_header(); ?>
 <div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <?php get_template_part("menu", "horizontal"); ?>
-        </div>
-    </div>
     <?php
         // Start the Loop.
         while ( have_posts() ) : the_post();
