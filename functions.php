fcfb873ed11b9b99d7c97cd87ab8db39a2264de2
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-04-15 11:36:22 -0300

    remove paginação do breadcrumb #56 Fix pensandoodireito/participacao-sitebase#56

--- functions.php
+++ functions.php
@@ -567,14 +567,7 @@ function wp_custom_breadcrumbs() {
             echo $before . 'Error 404' . $after;
         }
 
-        if ( get_query_var('paged') ) {
-            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
-            echo __('Page') . ' ' . get_query_var('paged');
-            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
-        }
-
         echo '</div>';
-
 
     }
 } // end wp_custom_breadcrumbs()
