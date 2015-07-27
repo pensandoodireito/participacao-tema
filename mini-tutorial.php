f9ccabaeb4dbe371846ebb852866e5d2df0ae5ad
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-07-27 20:55:50 -0300

    Reajuste de várias áreas: Melhorias nas notícias da home e nos títulos ( ); Melhorias no mini-tutorial; Melhorias na área de publicações na home

--- mini-tutorial.php
+++ mini-tutorial.php
@@ -1,45 +1,45 @@
 <div class="container mt-md">
     <div id="mini-tuto">
-        <h4 class="font-roboto red">Como participar?</h4>
-        <div class="row">
-            <div class="col-sm-3">
+        <h2 class="font-roboto red">Como participar?</h2>
+        <div class="row mt-md">
+            <div class="col-md-3 col-sm-6">
                 <div class="tuto-pass">
                     <div class="text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-01.png" class="img-adptive" alt="Cadastre-se no site">
-                        <p class="font-amatic h5">Cadastre-se no site</p>
+                        <p class="font-amatic h5">1 : Cadastre-se no site</p>
                     </div>
                     <div class="tuto-text">
-                        <p>É simples, e rápido.</p>
+                        <p>É simples e rápido</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-md-3 col-sm-6">
                 <div class="tuto-pass">
                     <div class="text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-02.png" class="img-adptive" alt="Confirme o cadastro">
-                        <p class="font-amatic h5">Confirme o cadastro</p>
+                        <p class="font-amatic h5">2 : Confirme o cadastro</p>
                     </div>
                     <div class="tuto-text">
                         <p>Vai chegar no seu email, é só clicar no link</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-md-3 col-sm-6">
                 <div class="tuto-pass">
                     <div class="text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-03.png" class="img-adptive" alt="Escolha">
-                        <p class="font-amatic h5">Escolha </p>
+                        <p class="font-amatic h5">3 : Escolha </p>
                     </div>
                     <div class="tuto-text">
                         <p>O debate que você quer participar</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-md-3 col-sm-6">
                 <div class="tuto-pass">
                     <div class="text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/tuto/tuto-04.png" class="img-adptive" alt="Cadastre-se no site">
-                        <p class="font-amatic h5">Pronto! </p>
+                        <p class="font-amatic h5">4 : Pronto! </p>
                     </div>
                     <div class="tuto-text">
                         <p>Deixe seu comentário, crítica etc</p>
