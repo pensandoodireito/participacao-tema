f7c3742f3f82781c2109e408a43ba18dc8fda9aa
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-07-31 10:58:38 -0300

    Remover "menu" antigo de debates;

--- single.php
+++ single.php
@@ -1,9 +1,6 @@
 <?php
 get_header(); ?>
     <div class="container">
-        <div class="row">
-            <?php get_template_part('menu', 'horizontal'); ?>
-        </div>
         <?php
         // Start the Loop.
         while (have_posts()) : the_post();
