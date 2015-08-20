383c1abba4f8b8bb74324b51ce1b7ab305201b22
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-08-20 18:35:54 -0300

    Refazer o header conforme novo padrão Alterações no header, imagens e icones das redes sociais;

--- js/participacao.js
+++ js/participacao.js
@@ -437,14 +437,22 @@ jQuery(document).ready(function(){
 );
 
 // torna o scroll na página suave, apenas usando o a classe .smoothscroll
-jQuery(function ($) {
+jQuery(function($) {
     $('.smoothscroll').on('click', function(event) {
-        var target = $( $(this).attr('href') );
-        if( target.length ) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: target.offset().top
             }, 600);
         }
     });
+    // Habilita o tooltip
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
 });
+
+
+
+
