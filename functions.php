0e0def0f8f1b4e3c70d694dd1ffc0e4d7dfc6036
Josafá Filho <josafafilho15@gmail.com>
2015-10-29 13:52:17 -0200

    Disponibiliza o widget de barra lateral padrão para todos os temas filhos do participacao

--- functions.php
+++ functions.php
@@ -815,13 +815,22 @@ function display_latest_news( $ignore = array() ) {
 
 function participacao_widgets_init() {
     register_sidebar( array(
+        'name'          => 'Barra Lateral',
+        'id'            => 'sidebar_widgets',
+        'before_widget' => '<div class="panel panel-default">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<div class="panel-heading"><h4 class="red font-roboto">',
+        'after_title'   => '</h4></div>',
+    ) );
+
+    register_sidebar( array(
         'name'          => 'Barra Lateral Interna',
         'id'            => 'barra-lateral-interna',
         'description'   => 'Itens a serem apresentados na barra lateral nas páginas internas',
-        'before_widget' => '<div id="%1$s" class="widget %2$s panel panel-default">',
+        'before_widget' => '<div class="panel panel-default">',
         'after_widget'  => '</div>',
         'before_title'  => '<div class="panel-heading"><h4 class="red font-roboto">',
-        'after_title'   => '</h2></div>',
+        'after_title'   => '</h4></div>',
     ) );
 }
 
