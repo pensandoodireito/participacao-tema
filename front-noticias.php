23f4e6e0a678b3170a9fd939aabdf54fb23bd183
Josafá Filho <josafafilho15@gmail.com>
2015-11-16 14:19:38 -0200

    Adiciona link para todas as notícias na capa

--- front-noticias.php
+++ front-noticias.php
@@ -5,6 +5,7 @@
 				<section class="noticias noticias-home">
 					<?php $sticky_posts = display_sticky_news(); ?>
 					<?php display_latest_news( $sticky_posts ); ?>
+					<a href="/noticias"><strong>Todas as Notícias</strong></a>
 				</section>
 			</div>
 			<div class="col-md-5">
