aa910e13e5c61d94feb5ac038d9fc58e0278212c
Josafá Filho <josafafilho15@gmail.com>
2016-01-21 11:12:16 -0200

    Corrige seleção de escopo do widget de imagem que era mantida ao tentar reaproveitar o seletor de midia ja usado

--- js/imagem-widget.js
+++ js/imagem-widget.js
@@ -10,14 +10,8 @@ jQuery(document).ready(function ($) {
 
         e.preventDefault();
 
-        //If the uploader object has already been created, reopen the dialog
-        if (uploader) {
-            uploader.open();
-            return;
-        }
-
         //Extend the wp.media object
-        uploader = wp.media.frames.file_frame = wp.media({
+        var uploader = wp.media.frames.file_frame = wp.media({
             title: 'Escolha a imagem',
             button: {
                 text: 'Escolha a imagem'
@@ -27,7 +21,7 @@ jQuery(document).ready(function ($) {
 
         //When a file is selected, grab the URL and set it as the text field's value
         uploader.on('select', function () {
-            attachment = uploader.state().get('selection').first().toJSON();
+            var attachment = uploader.state().get('selection').first().toJSON();
             $(target).val(attachment.url);
         });
 
